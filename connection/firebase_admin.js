5bcb08734ae8e46e07c087c158975816f3355f41
albert huang <d0191512@gmail.com>
2023-12-12 22:49:54 +0800

    remove secret code

--- connection/firebase_admin.js
+++
@@ -1,8 +0,0 @@
-const admin = require("firebase-admin");
-const serviceAccount = require("../group-97700-firebase-adminsdk-9efwg-adedffce2f.json");
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://group-97700-default-rtdb.firebaseio.com",
-  storageBucket:'gs://group-97700.appspot.com/'
-});
-module.exports = admin;
